b69c3741d505e28b9ae1b9d9d511470435da3b1d
Arnold Kigonya <arnold.kigonya@asigmacapital.com>
2023-06-26 22:05:59 +0300

    Cannot remember the changes

--- functions.py
+++ functions.py
@@ -5,42 +5,45 @@ Created on Thu May 25 19:59:24 2023
 @author: ArnoldKigonya
 """
 
-import streamlit as st
-from datetime import date
 import calendar
 import uuid
+from datetime import date
+
+import streamlit as st
+
 
 def get_years_since_2022():
     current_year = date.today().year
     return list(range(2022, current_year + 1))
 
+
 @st.cache_data
 def get_all_months():
     return list(calendar.month_name)[1:]
 
+
 @st.cache_data
 def get_all_names():
-    
     names = [
-        
         "Alvin Mulumba",
         "Edwin Mpoza",
         "Dennis Ssekimpi",
         "Arnold Kigonya",
         "Adrean Mugalaasi",
-        "Phillip Musumba"
+        "Phillip Musumba",
     ]
-    
+
     names.sort()
     return names
+
 
 def create_guid():
     guid = uuid.uuid4()
     return guid
+
 
 def get_key_by_value(dictionary, value):
     for key, val in dictionary.items():
         if val == value:
             return key
     return None
-
